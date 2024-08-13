902859d0ed2985f98d4e91fbc598ba2cc55a1981
Azfar Razzaq <azfarrazzaq23@gmail.com>
2024-08-13 18:24:30 +0500

    Formatted Individual Movie cards

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,7 +1,9 @@
 import "./MovieCard.css";
 import { useEffect, useState } from "react";
 import { fetchMovieData } from "../utility/utilityFunctions";
-
+import { FaStar } from "react-icons/fa6";
+import { FaStarHalf } from "react-icons/fa6";
+import { FaPlusCircle } from "react-icons/fa";
 export default function MovieCard({ movieId }: { movieId: string }) {
   const [movieDetails, setMovieDetails] = useState(undefined);
 
@@ -19,23 +21,22 @@ export default function MovieCard({ movieId }: { movieId: string }) {
         alt="Blade Runner"
       />
       <div className="movie-info">
-        <h3 className="movie-title">{movieDetails.Title}</h3>
-        <div className="movie-rating">
-          <span className="rating-star">⭐</span>
-          <span className="rating-value">{movieDetails.imdbRating}</span>
+        <div className="movie-title-container">
+          <h3 className="movie-title">{movieDetails.Title}</h3>
+          <div className="movie-rating">
+            <FaStar className="rating-star" />
+            <span className="rating-value">{movieDetails.imdbRating}</span>
+          </div>
         </div>
         <div className="movie-meta">
           <span className="movie-duration">{movieDetails.Runtime}</span>
           <span className="movie-genre">{movieDetails.Genre}</span>
+          <div className="watchlist">
+            <FaPlusCircle className="add-icon" /> Watchlist
+          </div>
         </div>
-        <a href="#" className="watchlist">
-          ➕ Watchlist
-        </a>
-        <p className="movie-description">
-          {movieDetails.Plot}
-        </p>
+        <p className="movie-description">{movieDetails.Plot}</p>
       </div>
-      <hr className="separator" />
     </div>
   ) : (
     <h1>LoadingMovie</h1>

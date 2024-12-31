ecfb494b0725cb104bfb414675d3ccd1eb0a6d08
Azfar <azfarrazzaq23@gmail.com>
2024-12-31 17:54:58 +0500

    Enhance MovieCard component to manage watchlist state; refactor inWatchList function and improve UI feedback for watchlist actions.

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -6,29 +6,24 @@ import { FaStar } from "react-icons/fa6";
 // import { FaStarHalf } from "react-icons/fa6";
 import { FaPlusCircle, FaMinusCircle } from "react-icons/fa";
 import { ThreeDots } from "react-loading-icons";
-
-type MovieDetails = {
-  Title: string;
-  Poster: string;
-  imdbRating: string;
-  Runtime: string;
-  Genre: string;
-  Plot: string;
-  imdbID: string;
-} & Record<string, string>;
-
+import { MovieDetails } from "../utility/customTypes";
 export default function MovieCard({ movieId }: { movieId: string }) {
   const [movieDetails, setMovieDetails] = useState<MovieDetails | undefined>(
     undefined
   );
-
+  const[isInWatchList, setIsInWatchList] = useState(false);
+  
   useEffect(() => {
     const apiKey = import.meta.env.VITE_API_KEY;
     // setMovieDetails(fetchMovieData(movieId,apiKey));
-    fetchMovieData(movieId, apiKey).then((data) => setMovieDetails(data));
+    fetchMovieData(movieId, apiKey).then((data) => {
+      setMovieDetails(data)
+      setIsInWatchList(inWatchList(data.imdbID));
+    
+    } );
   }, [movieId]);
 
-  function manageWatchlist(): void {
+  function manageWatchlist() {
     if (!movieDetails) {
       throw Error(
         "Add to Watchlist button clicked but movie details state is undefined"
@@ -46,6 +41,7 @@ export default function MovieCard({ movieId }: { movieId: string }) {
           (movieId) => movieId !== movieDetails.imdbID
         );
         localStorage.setItem("watchlist", JSON.stringify(newWatchListArray));
+        setIsInWatchList(false);
         return;
       }
     }
@@ -53,19 +49,14 @@ export default function MovieCard({ movieId }: { movieId: string }) {
     //case for addition
     watchlistArray.push(movieDetails.imdbID);
     localStorage.setItem("watchlist", JSON.stringify(watchlistArray));
+    setIsInWatchList(true);
   }
 
-  function inWatchList() {
-    if (!movieDetails) {
-      throw Error("Checking for watchlist while movieDetails is undefined");
-    }
-
-    let watchlistArray: string[] = [];
+  function inWatchList(imdbID: string) {
     const watchlistString = localStorage.getItem("watchlist");
 
     if (watchlistString) {
-      watchlistArray = JSON.parse(watchlistString);
-      if (watchlistArray.includes(movieDetails.imdbID)) {
+      if (JSON.parse(watchlistString).includes(imdbID)) {
         return true;
       }
     }
@@ -92,10 +83,10 @@ export default function MovieCard({ movieId }: { movieId: string }) {
           <span className="movie-duration">{movieDetails.Runtime}</span>
           <span className="movie-genre">{movieDetails.Genre}</span>
           <div className="watchlist" onClick={manageWatchlist}>
-            {inWatchList() ? (
-              <FaPlusCircle className="add-icon" />
-            ) : (
+            {isInWatchList ? (
               <FaMinusCircle className="add-icon" />
+            ) : (
+              <FaPlusCircle className="add-icon" />
             )}
           </div>
         </div>
